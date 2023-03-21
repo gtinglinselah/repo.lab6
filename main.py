3d43443b68a0e4b9a107b54fa0ac0be440e274ac
gtinglinselah <gtinglinselah@ufl.edu>
2023-03-21 11:43:16 -0400

    updated

--- main.py
+++ main.py
@@ -11,6 +11,33 @@ def encoder(pass_code):
     return password
 
 
+def menu():
+    print('\nOption Menu',
+          '-----------',
+          '1. Encode Password',
+          '2. Decode Password',
+          '3. Exit', sep='\n')
+
+def decoder(pass_code):
+    pass
+
+
 if __name__ == "__main__":
-    pass_code = str(input('Enter a password to encode: '))
-    print(encoder(pass_code))
+    coder = True
+
+    while coder:
+        password = None
+        menu()
+        option = int(input('\nPlease enter an option: '))
+
+        if option == 1:
+            pass_code = str(input('Please enter your password to encode: '))
+            pw_enc = encoder(pass_code)  # encoding password
+            password = pw_enc  # storing encoded password
+            print('Your password has been encoded and stored!')
+
+        elif option == 2:
+            pass
+
+        elif option == 3:
+            break
